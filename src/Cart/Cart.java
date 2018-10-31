6684fa89db7abda9fd04be79e8f259bb5b0045af
anunda51 <42669270+anunda51@users.noreply.github.com>
2018-10-31 14:49:09 +0700

    cart commit

--- src/Cart/Cart.java
+++ src/Cart/Cart.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Cart;
 
-/**
- *
- * @author Boss
- */
+import User.User;
+
 public class Cart {
+    private static String productname;
+    private static int size;
+    private static int quantity;
+    private static int price;
+    private static User user;
     
+    public Cart(String productname, int size, int quantity, int price, User user){
+        this.productname = productname;
+        this.size = size;
+        this.quantity = quantity;
+        this.price = price;
+        this.user = user;
+    }
+    public static String getProductname(){
+        return productname;
+    }public static int getSize(){
+        return size;
+    }public static int getQuantity(){
+        return quantity;
+    }public static int getPrice(){
+        return price;
+    }public static User getUser(){
+        return user;
+    }
 }
