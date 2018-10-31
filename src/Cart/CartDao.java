a63f45dce1e4df4ae9dae86471279403c55453f0
anunda51 <42669270+anunda51@users.noreply.github.com>
2018-10-31 16:40:54 +0700

    third commit

--- src/Cart/CartDao.java
+++ src/Cart/CartDao.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Cart;
 
-/**
- *
- * @author Boss
- */
+import static Connect.ConnectDB.connectDB;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import javax.swing.table.DefaultTableModel;
+
 public class CartDao {
-    
+    public DefaultTableModel getCart(){
+        try{
+            DB database = connectDB();
+            DBCollection collection = database.getCollection("cart");
+            DBCursor cursor = collection.find();
+            String[] columnname={"productname","size","quantity","price"};
+            DefaultTableModel model = new DefaultTableModel(columnname,0);
+            while(cursor.hasNext()){
+                DBObject obj = cursor.next();
+                String productname=(String) obj.get("productname").toString();
+                String size=(String) obj.get("size").toString();
+                String quantity=(String) obj.get("quantity").toString();
+                String price=(String) obj.get("price").toString();
+                
+                model.addRow(new Object[] {productname,size,quantity,price});
+            }
+            return model;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/Cart/FormCart.java
+++ src/Cart/FormCart.java
@@ -5,17 +5,17 @@
  */
 package Cart;
 
+import Index.FormIndex;
+
 /**
  *
  * @author Boss
  */
 public class FormCart extends javax.swing.JFrame {
-
-    /**
-     * Creates new form FormCart
-     */
+    CartService cart = new CartService();
     public FormCart() {
         initComponents();
+        cartTable.setModel(cart.showCartData());
     }
 
     /**
@@ -28,36 +28,127 @@ public class FormCart extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        cartTable = new javax.swing.JTable();
+        headerbar = new javax.swing.JPanel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setSize(new java.awt.Dimension(1100, 700));
+        setMaximumSize(new java.awt.Dimension(1920, 1080));
+        setPreferredSize(new java.awt.Dimension(1920, 1080));
+        setResizable(false);
+        setSize(new java.awt.Dimension(1920, 1080));
+        getContentPane().setLayout(null);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(null);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1100, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 700, Short.MAX_VALUE)
-        );
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setLayout(null);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Index/icon.png"))); // NOI18N
+        jPanel3.add(jLabel6);
+        jLabel6.setBounds(0, 0, 70, 60);
+
+        jLabel8.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel8.setFont(new java.awt.Font("Tahoma", 1, 28)); // NOI18N
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel8.setText("SPORTSHOP");
+        jLabel8.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel8.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel8MouseClicked(evt);
+            }
+        });
+        jPanel3.add(jLabel8);
+        jLabel8.setBounds(64, 0, 216, 60);
+
+        jPanel1.add(jPanel3);
+        jPanel3.setBounds(160, 40, 280, 60);
+        jPanel1.add(jTextField1);
+        jTextField1.setBounds(470, 50, 400, 40);
+
+        jButton1.setText("Search");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1);
+        jButton1.setBounds(920, 50, 100, 40);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Index/cart.png"))); // NOI18N
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(1170, 60, 40, 30);
+
+        cartTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Productname", "Size", "Quantity", "Price"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(cartTable);
+
+        jPanel1.add(jScrollPane1);
+        jScrollPane1.setBounds(160, 190, 1270, 402);
+
+        headerbar.setBackground(new java.awt.Color(0, 0, 0));
+        headerbar.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel1.add(headerbar);
+        headerbar.setBounds(160, 110, 1270, 50);
+
+        jButton2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jButton2.setText("Confirm");
+        jPanel1.add(jButton2);
+        jButton2.setBounds(1230, 680, 200, 70);
+
+        jButton3.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jButton3.setText("Back");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton3);
+        jButton3.setBounds(950, 680, 200, 70);
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 1630, 1030);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jLabel8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel8MouseClicked
+        
+    }//GEN-LAST:event_jLabel8MouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        FormIndex form = new FormIndex();
+        this.hide();
+        form.show();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -95,6 +186,17 @@ public class FormCart extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable cartTable;
+    private javax.swing.JPanel headerbar;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
